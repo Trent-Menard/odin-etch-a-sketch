eefd5aea3aaaee353efd4749467359ff2ca6b1ce
Trent Menard <trentmenard6@yahoo.com>
2023-08-01 18:37:42 -0500

    Create generateButtonsDiv(), update generateClearGridButton(), minor fixes, finish project!

--- GameHandler.js
+++ GameHandler.js
@@ -1,38 +1,76 @@
+generateButtonsDiv();
 generateCustomGridButton();
+generateClearGridButton();
 generateDivGrid();
 
 // Creates default 4x4 (16) grid; pass int to override size
 function generateDivGrid(dflt= 16) {
     let divElement = document.createElement("div");
-    divElement.setAttribute("id", "gridDiv");
+    divElement.id = "gridDiv";
     divElement.style.display = "grid";
-    divElement.style.gridTemplateColumns = "repeat(" + dflt + ", 1fr)";
-    divElement.style.gridTemplateRows = "repeat(" + dflt + ", 1fr)";
+    divElement.style.gridTemplateColumns = "repeat(" + dflt + ", auto)";
+    divElement.style.gridTemplateRows = "repeat(" + dflt + ", auto)";
+    divElement.style.border = "5px solid #590D22";
+
     document.getElementsByTagName("main")[0].appendChild(divElement);
 
     for (let i = 1; i <= dflt * dflt; i++) {
         let divElement = document.createElement("div");
         let divElementVal = document.createElement("p");
-        divElementVal.innerText = String(i);
-        divElement.setAttribute("id", "grid-" + i);
+        divElementVal.innerText = "";
+        divElement.id = "grid-" + i;
         divElement.setAttribute("class", "grid");
+        divElementVal.style.width = "1em";
+        divElementVal.style.height = "1em";
         document.getElementById("gridDiv").appendChild(divElement);
-        document.getElementById("grid-" + i).appendChild(divElementVal);
+
+        let targetDiv = document.getElementById("grid-" + i);
+        targetDiv.appendChild(divElementVal);
+        targetDiv.addEventListener("mouseover", () => {
+
+            if (targetDiv.style.backgroundColor === "")
+                targetDiv.style.backgroundColor="black";
+
+            if (targetDiv.style.opacity === "")
+                targetDiv.style.opacity = "0.1";
+
+            else if (targetDiv.style.opacity < 1.0)
+                targetDiv.style.opacity = (parseFloat(targetDiv.style.opacity) + 0.1).toString();
+        });
     }
+}
+
+// Creates "buttons-div"
+function generateButtonsDiv() {
+    let divElement = document.createElement("div");
+    divElement.setAttribute("id", "buttons-div");
+    document.getElementsByTagName("main")[0].appendChild(divElement);
 }
 
 // Creates "Create Custom Grid" button & handler
 function generateCustomGridButton() {
-    let divElement = document.createElement("div");
-    divElement.setAttribute("id", "custom-grid-div");
-    document.getElementsByTagName("main")[0].appendChild(divElement);
-
     let buttonElement = document.createElement("button");
     buttonElement.setAttribute("id", "custom-grid-button");
     buttonElement.textContent = "Create Custom Grid";
-    let customGridDivElement = document.getElementById("custom-grid-div");
-    customGridDivElement.appendChild(buttonElement);
-    customGridDivElement.addEventListener("click", () => handleUserInput());
+
+    let buttonsDivElement = document.getElementById("buttons-div");
+    buttonsDivElement.appendChild(buttonElement);
+
+    document.getElementById("custom-grid-button").addEventListener("click", () => handleUserInput());
+}
+
+// Creates "Clear Grid" button & handler
+function generateClearGridButton() {
+    let buttonElement = document.createElement("button");
+    buttonElement.setAttribute("id", "clear-grid-button");
+    buttonElement.textContent = "Clear Grid";
+
+    let buttonsDivElement = document.getElementById("buttons-div");
+    buttonsDivElement.appendChild(buttonElement);
+
+    document.getElementById("clear-grid-button").addEventListener("click", () => {
+        document.querySelectorAll(".grid").forEach((x) => x.removeAttribute("style"));
+    });
 }
 
 // Parse user input and replace current grid size with input
